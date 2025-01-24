6a2de506cf07ae141c31bf8446bc779588b5d64a
wlqg1983 <40624479+wlqg1983@users.noreply.github.com>
2025-01-25 00:32:57 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -3,5 +3,4 @@
 cp -r bin/plasmidrender $(conda info --base)/envs/MiRI_MiRIV_1.0/lib/python3.12/site-packages/
 rm -r bin/plasmidrender
 chmod +x bin/*
-cp bin/* $(conda info --base)/envs/MiRI_MiRIV_1.0/bin
 rm MiRI_MiRIV_1.0.yml
